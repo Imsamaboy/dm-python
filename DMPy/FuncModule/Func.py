02ee99178809a6d588fe576f5c1bb5adcc1f75c5
GinGin3203 <ginzburggeorgy@gmail.com>
2020-05-21 00:05:40 +0300

    Форматирование кода в соответствие с PEP8

--- DMPy/FuncModule/Func.py
+++ DMPy/FuncModule/Func.py
@@ -1,4 +1,5 @@
-""" Модуль для работы со всюду определенными (тотальными) целочисленными функциями"""
+""" Модуль для работы со всюду определенными (тотальными)
+целочисленными функциями"""
 from DataStructures.Carrier import Carrier
 from DataStructures.DMGraph import DMGraph
 from typing import Iterable, List, Tuple
@@ -9,15 +10,18 @@ class Func:
 
     def __init__(self, func=lambda x: x):
         """
-        :param func: принимаемая функция. По умолчанию стоит тождественное отображение
+        :param func: принимаемая функция. По умолчанию стоит тождественное
+            отображение
         """
         self.func = func
         self.domain = None
 
-    """TODO: Переделать перегрузить метод __call__ (т.е. создать 2 метода __call__, один для типа 
+    """TODO: Переделать перегрузить метод __call__ (т.е. создать 2 метода 
+    __call__, один для типа 
     Carrier и другой для всего остального) с помощью декораторов
-
-    Туториал: https://www.codementor.io/@arpitbhayani/overload-functions-in-python-13e32ahzqt """
+    
+    Туториал: https://www.codementor.io/@arpitbhayani/overload-functions-
+    in-python-13e32ahzqt """
 
     def __call__(self, arg: Carrier or DMGraph):
         """ __call__ позволяет такой синтаксис:
@@ -33,17 +37,23 @@ class Func:
         return self.func(arg)
 
     """TODO:
-    Перегрузить метод (т.е. создать с таким же названием, но другой) (опять же с помощью декоратора) так, чтобы его можно было применять не только к типу Carrier,
+    Перегрузить метод (т.е. создать с таким же названием, но другой) 
+    (опять же с помощью декоратора) так, чтобы его можно было применять 
+    не только к типу Carrier,
     но и к одному элементу"""
 
-    def generate_orbit(self, domain: Carrier, depth=3, shape='matrix') -> Carrier:
+    def generate_orbit(self, domain: Carrier, depth=3,
+                       shape='matrix') -> Carrier:
         """
-    Метод для составления орбит элементов относительно данной функции (т.е. f(f(f..(f(x))..)) )
+    Метод для составления орбит элементов относительно данной функции
+    (т.е. f(f(f..(f(x))..)) )
 
-    Если shape='matrix', возвращает Carrier, элементами которого являются другие Carrier'ы (матрица короче), где return_value[0] - это domain,
+    Если shape='matrix', возвращает Carrier, элементами которого являются
+    другие Carrier'ы (матрица короче), где return_value[0] - это domain,
     return_value[1] - f(domain), return_value[2] - f(f(domain)) и т.д.
 
-    Если shape='flat', то возвращает одномерный Carrier, где return_value[i] = f(f..(f(domain[i]))
+    Если shape='flat', то возвращает одномерный Carrier,
+    где return_value[i] = f(f..(f(domain[i]))
     :param domain: множество действия функции
     :param depth: сколько раз мы применяем функцию к предыдущему результату
     :param shape: 'matrix', 'flat'
@@ -54,9 +64,9 @@ class Func:
         if type(arg) == Carrier:
             return [(x, im) for x, im in zip(arg, map(self.func, arg))]
         elif type(arg) == DMGraph:
-            return [(x, im) for x, im in zip(arg.nodes, map(self.func, arg.nodes))]
+            return [(x, im) for x, im in
+                    zip(arg.nodes, map(self.func, arg.nodes))]
 
-
-if __name__ == "__main__":
-    f = Func(func=lambda x: x ** 2)
-    print(f(Carrier(n=5)))
+# if __name__ == "__main__":
+#     f = Func(func=lambda x: x ** 2)
+#     print(f(Carrier(n=5)))
