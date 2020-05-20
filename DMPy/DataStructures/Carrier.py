9c3cd961459c08074eea83b70d3f99288526d856
GinGin3203 <ginzburggeorgy@gmail.com>
2020-05-20 05:21:14 +0300

    Updated README + minor changes

--- DMPy/DataStructures/Carrier.py
+++ DMPy/DataStructures/Carrier.py
@@ -1,35 +1,34 @@
-""" Carrier будет дефолтным типом данных для наших операций. """
+""" Carrier будет стандартный типом данных для наших манипуляций. """
 
 from collections.abc import MutableSet
 
 """ Основная спецификация: 
 - можно итерироваться по объекту
-- должен поддерживать оператор [] (элемент по индексу)"""
-
+- 
+"""
 
 class Carrier(MutableSet):
     """ Стандартный тип данных пакета """
 
-    def __init__(self, init = None):
-        """
-
-        :param data: инициализация либо через set, либо через int. В случае int создает
-        лист из чисел 1..init. Можно не указывать init, тогда создастся пустое множество
+    def __init__(self, init=None):
+        """ :param data: инициализация либо через set, либо через int. В случае int создает
+        объект из чисел 1..init. Можно не указывать init, тогда создастся пустое множество
         Экземпляр класса можно распечатать, используя print
-        """
 
+        """
         MutableSet.__init__(self)
 
-        if init == None:
+        if not init:
             self.__data = set({})
         elif type(init) == int:
-            self.__data = set([i in range(1, init+1)])
+            self.__data = {i in range(1, init + 1)}
         else:
             self.__data = set(init)
 
     ''' MutableSet - абстрактный класс, так что мы должны определить для нашего носителя оперции __contains__ ( оператор in),
     __iter__ ( для терирования по множеству в цикле ), __len__ для поддержки оператора len, add и discard для операций на множестве
     '''
+
     def __contains__(self, key):
         return key in self.__data
 
@@ -46,7 +45,8 @@ class Carrier(MutableSet):
         self.__data.discard(key)
 
     def __str__(self):
-        return str(self.__data) #Реализвция __str__ позволяет распечатать обьект, используя print, а также преобразовать его в строку
+        return str(
+            self.__data)  # Реализвция __str__ позволяет распечатать обьект, используя print, а также преобразовать его в строку
 
 
 # Тест
@@ -66,4 +66,3 @@ if __name__ == "__main__":
     print(car)
     car.add(0)
     print(car)
-
