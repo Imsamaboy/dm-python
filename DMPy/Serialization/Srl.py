2795a13bd40e2bfbc1d3a0bc3883337b6d3af22a
kairoslav <svt210@yandex.ru>
2020-05-27 01:22:02 +0300

    Rename functions
    According to TR.md

--- DMPy/Serialization/Srl.py
+++ DMPy/Serialization/Srl.py
@@ -2,7 +2,7 @@ import pickle
 from DataStructures.Carrier import Carrier
 
 
-def dump(something, filename):
+def toStr(something, filename):
     """
     Функция сериализации объектов в памяти,
     принимаемые аргументы - сам объект и Ваше название для файла.
@@ -11,7 +11,7 @@ def dump(something, filename):
         pickle.dump(something, file)
 
 
-def load(filename):
+def fromStr(filename):
     """ Загрузка объекта из памяти, принимаемый аргумент - название файла."""
     with open(rf'{filename}.pickle', 'rb') as file:
         return pickle.load(file)
