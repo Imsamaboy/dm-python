2265e00a616f97e57e24f30bcdd82c7907ee8dd0
GinGin3203 <ginzburggeorgy@gmail.com>
2020-05-26 23:57:34 +0300

    DMPy Reworked Support to be an associative container

--- DMPy/FuncModule/Func.py
+++ DMPy/FuncModule/Func.py
@@ -1,8 +1,9 @@
 """ Модуль для работы со всюду определенными (тотальными)
 целочисленными функциями"""
-from DataStructures.Carrier import Carrier
-from DataStructures.DMGraph import DMGraph
 from typing import Iterable, List, Tuple
+
+from DataStructures.DMGraph import DMGraph
+from DataStructures.Support import Support
 
 
 class Func:
@@ -18,20 +19,20 @@ class Func:
 
     """TODO: Переделать перегрузить метод __call__ (т.е. создать 2 метода 
     __call__, один для типа 
-    Carrier и другой для всего остального) с помощью декораторов
+    Support и другой для всего остального) с помощью декораторов
     
     Туториал: https://www.codementor.io/@arpitbhayani/overload-functions-
     in-python-13e32ahzqt """
 
-    def __call__(self, arg: Carrier or DMGraph):
+    def __call__(self, arg: Support or DMGraph):
         """ __call__ позволяет такой синтаксис:
 
         new_fun = Func(func=x**2)  # Инициализируем объект класса Func
         result = new_fun(3) # result = 9
 
     """
-        if type(arg) == Carrier:
-            return Carrier(map(self.func, arg))
+        if type(arg) == Support:
+            return Support(map(self.func, arg))
         elif type(arg) == DMGraph:
             return DMGraph(self._arg_image_pairs(arg.nodes))
         return self.func(arg)
@@ -39,20 +40,20 @@ class Func:
     """TODO:
     Перегрузить метод (т.е. создать с таким же названием, но другой) 
     (опять же с помощью декоратора) так, чтобы его можно было применять 
-    не только к типу Carrier,
+    не только к типу Support,
     но и к одному элементу"""
 
-    def generate_orbit(self, domain: Carrier, depth=3,
-                       shape='matrix') -> Carrier:
+    def generate_orbit(self, domain: Support, depth=3,
+                       shape='matrix') -> Support:
         """
     Метод для составления орбит элементов относительно данной функции
     (т.е. f(f(f..(f(x))..)) )
 
-    Если shape='matrix', возвращает Carrier, элементами которого являются
-    другие Carrier'ы (матрица короче), где return_value[0] - это domain,
+    Если shape='matrix', возвращает Support, элементами которого являются
+    другие Support'ы (матрица короче), где return_value[0] - это domain,
     return_value[1] - f(domain), return_value[2] - f(f(domain)) и т.д.
 
-    Если shape='flat', то возвращает одномерный Carrier,
+    Если shape='flat', то возвращает одномерный Support,
     где return_value[i] = f(f..(f(domain[i]))
     :param domain: множество действия функции
     :param depth: сколько раз мы применяем функцию к предыдущему результату
@@ -61,7 +62,7 @@ class Func:
         pass
 
     def _arg_image_pairs(self, arg: Iterable or DMGraph) -> List[Tuple]:
-        if type(arg) == Carrier:
+        if type(arg) == Support:
             return [(x, im) for x, im in zip(arg, map(self.func, arg))]
         elif type(arg) == DMGraph:
             return [(x, im) for x, im in
