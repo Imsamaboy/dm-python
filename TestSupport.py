d044d028560bd1e75ae4e50232e008d431613c1f
Alex-M-coder <55296215+Alex-M-coder@users.noreply.github.com>
2020-06-05 00:06:47 +0300

    Updated tests for Support and Operation modules
    small improvements

--- TestSupport.py
+++ TestSupport.py
@@ -1,13 +1,15 @@
 import unittest
 from DMpy.Support import Support
 class TestSupport(unittest.TestCase):
-  def setUp(self):
+
+  def test_forth_for_strings(self):
     self.support = Support({'котик', 'жабка', 'выдра'})
-  def test_forth(self):
     self.assertFalse(self.support.forth('котик') == self.support.forth('жабка'))
     self.assertFalse(self.support.forth('котик') == self.support.forth('выдра'))
     self.assertFalse(self.support.forth('жабка') == self.support.forth('выдра'))
-  def test_back(self):
+
+  def test_back_for_strings(self):
+    self.support = Support({'котик', 'жабка', 'выдра'})
     self.assertFalse(self.support.back(1) == self.support.back(2))
     self.assertFalse(self.support.back(1) == self.support.back(3))
     self.assertFalse(self.support.back(2) == self.support.back(3))
@@ -15,5 +17,29 @@ class TestSupport(unittest.TestCase):
     self.assertEqual(self.support.back(self.support.forth('жабка')), 'жабка')
     self.assertEqual(self.support.back(self.support.forth('выдра')), 'выдра')
 
-if __name__ == "__main__":
-  unittest.main()
+
+  def test_forth_for_numbers(self):
+    self.support = Support({11, 22, 33})
+    self.assertFalse(self.support.forth(11) == self.support.forth(22))
+    self.assertFalse(self.support.forth(11) == self.support.forth(33))
+    self.assertFalse(self.support.forth(22) == self.support.forth(33))
+
+  def test_back_for_numbers(self):
+    self.support = Support({11, 22, 33})
+    self.assertFalse(self.support.back(1) == self.support.back(2))
+    self.assertFalse(self.support.back(1) == self.support.back(3))
+    self.assertFalse(self.support.back(2) == self.support.back(3))
+    self.assertEqual(self.support.back(self.support.forth(11)), 11)
+    self.assertEqual(self.support.back(self.support.forth(22)), 22)
+    self.assertEqual(self.support.back(self.support.forth(33)), 33)
+
+
+  def test_forth_for_boolean(self):
+    self.support = Support({True, False})
+    self.assertFalse(self.support.forth(True) == self.support.forth(False))
+
+  def test_back_for_boolean(self):
+    self.support = Support({True, False})
+    self.assertFalse(self.support.back(1) == self.support.back(2))
+    self.assertEqual(self.support.back(self.support.forth(True)), True)
+    self.assertEqual(self.support.back(self.support.forth(False)), False)
