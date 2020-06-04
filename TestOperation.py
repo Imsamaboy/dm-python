50beec9aafbe0e930c78892ded51a5217e189635
Alex-M-coder <55296215+Alex-M-coder@users.noreply.github.com>
2020-06-05 01:46:37 +0300

    TestSupport and TestOperation renaming and cleaning

--- TestOperation.py
+++ TestOperation.py
@@ -7,110 +7,125 @@ class TestOperation(unittest.TestCase):
   def divide_ints(a, b):
     return a / b
 
-  def test__contains__for_graph_initialisation(self):
+"""__contains__ method is tested here (initialisation by graph)"""
+  def test_checking_for_membership____graph(self):
     g1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper1 = Operation(graph = g1)
+    oper1 = Operation(graph = g1)
     for k in g1
-      self.assertTrue((k, g1[k]) in self.oper1)
-      self.assertFalse((k, -1) in self.oper1)
-    self.assertFalse(((1, 3), 1) in self.oper1)
+      self.assertTrue((k, g1[k]) in oper1)
+      self.assertFalse((k, -1) in oper1)
+    self.assertFalse(((1, 3), 1) in oper1)
 
-  def test__contains__for_procedure_initialisation(self):
-    self.oper1 = Operation(procedure = sum_ints)
+"""__contains__ method is tested here (initialisation by procedure)"""
+  def test_checking_for_membership____procedure(self):
+    oper1 = Operation(procedure = sum_ints)
     for a in range(10)
       for b in range(10)
-        self.assertTrue(((a, b), a+b) in self.oper1)
-        self.assertFalse(((a, b), a+b+1) in self.oper1)
+        self.assertTrue(((a, b), a+b) in oper1)
+        self.assertFalse(((a, b), a+b+1) in oper1)
 
-  def test__iter__(self):
+
+"""__iter__ method is tested here (initialisation by graph)"""
+  def test_iterator_creating____graph(self):
     g1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper1 = Operation(graph = g1)
+    oper1 = Operation(graph = g1)
 
     unchecked_tuples = [k for k in g1]
-    for i in self.oper1
-      self.assertEqual(g1[i[0]], i[1])
-      self.assertTrue(i[0] in unchecked_tuples)
-      if i[0] in unchecked_tuples:
-        unchecked_tuples.remove(i[0])
+    for var_res in oper1
+      self.assertEqual(g1[var_res[0]], var_res[1])
+      self.assertTrue(var_res[0] in unchecked_tuples)
+      if var_res[0] in unchecked_tuples:
+        unchecked_tuples.remove(var_res[0])
       else:
         break
 
-  def test__call__for_graph_initialisation(self):
+
+"""__call__ method is tested here (initialisation by graph)"""
+  def test_calling_operation_result____graph(self):
     g1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper1 = Operation(graph = g1)
+    oper1 = Operation(graph = g1)
 
     results_of_operations = []
     results_from_graph = []
     for k in g1
-      results_of_operations.append(self.oper1(k[0], k[1]))
+      results_of_operations.append(oper1(k[0], k[1]))
       results_from_graph.append(g1[k])
     self.assertEqual(results_of_operations, results_from_graph)
 
-  def test__call__for_procedure_initialisation(self):
-    self.oper2 = Operation(procedure = sum_ints)
+"""__call__ method is tested here (initialisation by procedure)"""
+  def test_calling_operation_result____procedure(self):
+    oper2 = Operation(procedure = sum_ints)
     for a in range(100)
       for b in range(100)
-        self.assertTrue(self.oper2(a, b), a+b)
-        self.assertFalse(self.oper2(a, b), a+b+1)
+        self.assertTrue(oper2(a, b), a+b)
+        self.assertFalse(oper2(a, b), a+b+1)
+
 
   def test_isTotal(self):
     """need help"""
 
-  def test_isAssociative_for_graph_initialisation(self):
+
+"""isAssociative method is tested here (initialisation by graph)"""
+  def test_associativity_checking____graph(self):
     g1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper1 = Operation(graph = g1)
+    oper1 = Operation(graph = g1)
     g2 = {(0, 0): 0, (0, 1): 2, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper2 = Operation(graph = g2)
-    self.assertTrue(self.oper1.isAssociative())
-    self.assertFalse(self.oper2.isAssociative())
+    oper2 = Operation(graph = g2)
+    self.assertTrue(oper1.isAssociative())
+    self.assertFalse(oper2.isAssociative())
 
-  def test_isAssociative_for_procedure_initialisation(self):
-    self.oper1 = Operation(procedure = sum_ints)
-    self.oper2 = Operation(procedure = divide_ints)
-    self.assertTrue(self.oper1.isAssociative())
-    self.assertFalse(self.oper2.isAssociative())
+"""isAssociative method is tested here (initialisation by procedure)"""
+  def test_associativity_checking____procedure(self):
+    oper1 = Operation(procedure = sum_ints)
+    oper2 = Operation(procedure = divide_ints)
+    self.assertTrue(oper1.isAssociative())
+    self.assertFalse(oper2.isAssociative())
 
 
-  def test_isCommutative_for_graph_initialisation(self):
+"""isCommutative method is tested here (initialisation by graph)"""
+  def test_commutativity_checking____graph(self):
     g1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper1 = Operation(graph = g1)
+    oper1 = Operation(graph = g1)
     g2 = {(0, 0): 0, (0, 1): 2, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper2 = Operation(graph = g2)
-    self.assertTrue(self.oper1.isCommutative())
-    self.assertFalse(self.oper2.isCommutative())
+    oper2 = Operation(graph = g2)
+    self.assertTrue(oper1.isCommutative())
+    self.assertFalse(oper2.isCommutative())
 
-  def test_isCommutative_for_procedure_initialisation(self):
-    self.oper1 = Operation(procedure = sum_ints)
-    self.oper2 = Operation(procedure = divide_ints)
-    self.assertTrue(self.oper1.isCommutative())
-    self.assertFalse(self.oper2.isCommutative())
+"""isCommutative method is tested here (initialisation by procedure)"""
+  def test_commutativity_checking____procedure(self):
+    oper1 = Operation(procedure = sum_ints)
+    oper2 = Operation(procedure = divide_ints)
+    self.assertTrue(oper1.isCommutative())
+    self.assertFalse(oper2.isCommutative())
 
-
-  def test_hasNeutral_for_graph_initialisation(self):
+"""hasNeutral method is tested here (initialisation by graph)"""
+  def test_neutral_elem_existance____graph(self):
     g1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper1 = Operation(graph = g1)
+    oper1 = Operation(graph = g1)
     g2 = {(0, 0): 0, (0, 1): 2, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper2 = Operation(graph = g2)
-    self.assertTrue(self.oper1.hasNeutral())
-    self.assertFalse(self.oper2.hasNeutral())
+    oper2 = Operation(graph = g2)
+    self.assertTrue(oper1.hasNeutral())
+    self.assertFalse(oper2.hasNeutral())
 
-  def test_hasNeutral_for_procedure_initialisation(self):
-    self.oper1 = Operation(procedure = sum_ints)
-    self.oper2 = Operation(procedure = divide_ints)
-    self.assertTrue(self.oper1.hasNeutral())
-    self.asserttrue(self.oper2.hasNeutral())
+"""hasNeutral method is tested here (initialisation by procedure)"""
+  def test_neutral_elem_existance____procedure(self):
+    oper1 = Operation(procedure = sum_ints)
+    oper2 = Operation(procedure = divide_ints)
+    self.assertTrue(oper1.hasNeutral())
+    self.asserttrue(oper2.hasNeutral())
 
-
-  def test_hasInverse_for_graph_initialisation(self):
+"""hasInverse method is tested here (initialisation by graph)"""
+  def test_inverse_elem_existance____graph(self):
     g1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    self.oper1 = Operation(graph = g1)
+    oper1 = Operation(graph = g1)
     g2 = {(0, 0): 0, (0, 1): 1, (1, 0): 1, (1, 1): 1}
-    self.oper2 = Operation(graph = g2)
-    self.assertTrue(self.oper1.hasInverse())
-    self.assertFalse(self.oper2.hasInverse())
+    oper2 = Operation(graph = g2)
+    self.assertTrue(oper1.hasInverse())
+    self.assertFalse(oper2.hasInverse())
 
-  def test_hasInverse_for_procedure_initialisation(self):
-    self.oper1 = Operation(procedure = sum_ints)
-    self.oper2 = Operation(procedure = divide_ints)
-    self.assertTrue(self.oper1.hasInverse())
-    self.assertFalse(self.oper2.hasInverse())
+"""hasInverse method is tested here (initialisation by procedure)"""
+  def test_inverse_elem_existance____procedure(self):
+    oper1 = Operation(procedure = sum_ints)
+    oper2 = Operation(procedure = divide_ints)
+    self.assertTrue(oper1.hasInverse())
+    self.assertFalse(oper2.hasInverse())
--- TestSupport.py
+++ TestSupport.py
@@ -3,58 +3,58 @@ from DMpy.Support import Support
 class TestSupport(unittest.TestCase):
 
   def test_forth_for_strings(self):
-    self.support = Support({'котик', 'жабка', 'выдра'})
-    self.assertFalse(self.support.forth('котик') == self.support.forth('жабка'))
-    self.assertFalse(self.support.forth('котик') == self.support.forth('выдра'))
-    self.assertFalse(self.support.forth('жабка') == self.support.forth('выдра'))
+    support = Support({'котик', 'жабка', 'выдра'})
+    self.assertFalse(support.forth('котик') == support.forth('жабка'))
+    self.assertFalse(support.forth('котик') == support.forth('выдра'))
+    self.assertFalse(support.forth('жабка') == support.forth('выдра'))
 
   def test_back_for_strings(self):
-    self.support = Support({'котик', 'жабка', 'выдра'})
-    self.assertFalse(self.support.back(1) == self.support.back(2))
-    self.assertFalse(self.support.back(1) == self.support.back(3))
-    self.assertFalse(self.support.back(2) == self.support.back(3))
-    self.assertEqual(self.support.back(self.support.forth('котик')), 'котик')
-    self.assertEqual(self.support.back(self.support.forth('жабка')), 'жабка')
-    self.assertEqual(self.support.back(self.support.forth('выдра')), 'выдра')
+    support = Support({'котик', 'жабка', 'выдра'})
+    self.assertFalse(support.back(1) == support.back(2))
+    self.assertFalse(support.back(1) == support.back(3))
+    self.assertFalse(support.back(2) == support.back(3))
+    self.assertEqual(support.back(support.forth('котик')), 'котик')
+    self.assertEqual(support.back(support.forth('жабка')), 'жабка')
+    self.assertEqual(support.back(support.forth('выдра')), 'выдра')
 
 
   def test_forth_for_numbers(self):
-    self.support = Support({11, 22, 33})
-    self.assertFalse(self.support.forth(11) == self.support.forth(22))
-    self.assertFalse(self.support.forth(11) == self.support.forth(33))
-    self.assertFalse(self.support.forth(22) == self.support.forth(33))
+    support = Support({11, 22, 33})
+    self.assertFalse(support.forth(11) == support.forth(22))
+    self.assertFalse(support.forth(11) == support.forth(33))
+    self.assertFalse(support.forth(22) == support.forth(33))
 
   def test_back_for_numbers(self):
-    self.support = Support({11, 22, 33})
-    self.assertFalse(self.support.back(1) == self.support.back(2))
-    self.assertFalse(self.support.back(1) == self.support.back(3))
-    self.assertFalse(self.support.back(2) == self.support.back(3))
-    self.assertEqual(self.support.back(self.support.forth(11)), 11)
-    self.assertEqual(self.support.back(self.support.forth(22)), 22)
-    self.assertEqual(self.support.back(self.support.forth(33)), 33)
+    support = Support({11, 22, 33})
+    self.assertFalse(support.back(1) == support.back(2))
+    self.assertFalse(support.back(1) == support.back(3))
+    self.assertFalse(support.back(2) == support.back(3))
+    self.assertEqual(support.back(support.forth(11)), 11)
+    self.assertEqual(support.back(support.forth(22)), 22)
+    self.assertEqual(support.back(support.forth(33)), 33)
 
 
   def test_forth_for_boolean(self):
-    self.support = Support({True, False})
-    self.assertFalse(self.support.forth(True) == self.support.forth(False))
+    support = Support({True, False})
+    self.assertFalse(support.forth(True) == support.forth(False))
 
   def test_back_for_boolean(self):
-    self.support = Support({True, False})
-    self.assertFalse(self.support.back(1) == self.support.back(2))
-    self.assertEqual(self.support.back(self.support.forth(True)), True)
-    self.assertEqual(self.support.back(self.support.forth(False)), False)
+    support = Support({True, False})
+    self.assertFalse(support.back(1) == support.back(2))
+    self.assertEqual(support.back(support.forth(True)), True)
+    self.assertEqual(support.back(support.forth(False)), False)
 
   def test_forth_for_mixed_types(self):
-    self.support = Support({'котик', 0, True})
-    self.assertFalse(self.support.forth('котик') == self.support.forth(0))
-    self.assertFalse(self.support.forth('котик') == self.support.forth(True))
-    self.assertFalse(self.support.forth(0) == self.support.forth(True))
+    support = Support({'котик', 0, True})
+    self.assertFalse(support.forth('котик') == support.forth(0))
+    self.assertFalse(support.forth('котик') == support.forth(True))
+    self.assertFalse(support.forth(0) == support.forth(True))
 
   def test_back_for_mixed_types(self):
-    self.support = Support({'котик', 0, True})
-    self.assertFalse(self.support.back(1) == self.support.back(2))
-    self.assertFalse(self.support.back(1) == self.support.back(3))
-    self.assertFalse(self.support.back(2) == self.support.back(3))
-    self.assertEqual(self.support.back(self.support.forth('котик')), 'котик')
-    self.assertEqual(self.support.back(self.support.forth(0)), 0)
-    self.assertEqual(self.support.back(self.support.forth(True)), True)
+    support = Support({'котик', 0, True})
+    self.assertFalse(support.back(1) == support.back(2))
+    self.assertFalse(support.back(1) == support.back(3))
+    self.assertFalse(support.back(2) == support.back(3))
+    self.assertEqual(support.back(support.forth('котик')), 'котик')
+    self.assertEqual(support.back(support.forth(0)), 0)
+    self.assertEqual(support.back(support.forth(True)), True)
