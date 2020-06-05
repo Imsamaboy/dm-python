373e57882912719545e2755ee599baf0386ccf2b
Alex-M-coder <55296215+Alex-M-coder@users.noreply.github.com>
2020-06-05 18:33:01 +0300

    Renaming Operation class into Oper

--- TestOperation.py
+++ TestOperation.py
@@ -1,6 +1,6 @@
 import unittest
-from DMpy.Operations import Operation
-class TestOperation(unittest.TestCase):
+from DMpy.Operations import Oper
+class TestOper(unittest.TestCase):
   def sum_ints(a, b):
     return a + b
 
@@ -10,7 +10,7 @@ class TestOperation(unittest.TestCase):
 """__contains__ method is tested here (initialisation by graph)"""
   def test_checking_for_membership_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper1 = Operation(graph = graph1)
+    oper1 = Oper(graph = graph1)
     for key in graph1
       self.assertTrue((key, graph1[key]) in oper1)
       self.assertFalse((key, -1) in oper1)
@@ -18,7 +18,7 @@ class TestOperation(unittest.TestCase):
 
 """__contains__ method is tested here (initialisation by procedure)"""
   def test_checking_for_membership_in_operation_initialised_by_procedure(self):
-    oper1 = Operation(procedure = sum_ints)
+    oper1 = Oper(procedure = sum_ints)
     for a in range(10)
       for b in range(10)
         self.assertTrue(((a, b), a+b) in oper1)
@@ -28,7 +28,7 @@ class TestOperation(unittest.TestCase):
 """__iter__ method is tested here (initialisation by graph)"""
   def test_iterator_creating_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper1 = Operation(graph = graph1)
+    oper1 = Oper(graph = graph1)
 
     unchecked_tuples = [key for key in graph1]
     for var_res in oper1
@@ -43,7 +43,7 @@ class TestOperation(unittest.TestCase):
 """__call__ method is tested here (initialisation by graph)"""
   def test_calling_operation_result_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper1 = Operation(graph = graph1)
+    oper1 = Oper(graph = graph1)
 
     results_of_operations = []
     results_from_graph = []
@@ -54,7 +54,7 @@ class TestOperation(unittest.TestCase):
 
 """__call__ method is tested here (initialisation by procedure)"""
   def test_calling_operation_result_in_operation_initialised_by_procedure(self):
-    oper2 = Operation(procedure = sum_ints)
+    oper2 = Oper(procedure = sum_ints)
     for a in range(100)
       for b in range(100)
         self.assertTrue(oper2(a, b), a+b)
@@ -68,16 +68,16 @@ class TestOperation(unittest.TestCase):
 """isAssociative method is tested here (initialisation by graph)"""
   def test_associativity_checking_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper1 = Operation(graph = graph1)
+    oper1 = Oper(graph = graph1)
     graph2 = {(0, 0): 0, (0, 1): 2, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper2 = Operation(graph = graph2)
+    oper2 = Oper(graph = graph2)
     self.assertTrue(oper1.isAssociative())
     self.assertFalse(oper2.isAssociative())
 
 """isAssociative method is tested here (initialisation by procedure)"""
   def test_associativity_checking_in_operation_initialised_by_procedure(self):
-    oper1 = Operation(procedure = sum_ints)
-    oper2 = Operation(procedure = divide_ints)
+    oper1 = Oper(procedure = sum_ints)
+    oper2 = Oper(procedure = divide_ints)
     self.assertTrue(oper1.isAssociative())
     self.assertFalse(oper2.isAssociative())
 
@@ -85,47 +85,47 @@ class TestOperation(unittest.TestCase):
 """isCommutative method is tested here (initialisation by graph)"""
   def test_commutativity_checking_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper1 = Operation(graph = graph1)
+    oper1 = Oper(graph = graph1)
     graph2 = {(0, 0): 0, (0, 1): 2, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper2 = Operation(graph = graph2)
+    oper2 = Oper(graph = graph2)
     self.assertTrue(oper1.isCommutative())
     self.assertFalse(oper2.isCommutative())
 
 """isCommutative method is tested here (initialisation by procedure)"""
   def test_commutativity_checking_in_operation_initialised_by_procedure(self):
-    oper1 = Operation(procedure = sum_ints)
-    oper2 = Operation(procedure = divide_ints)
+    oper1 = Oper(procedure = sum_ints)
+    oper2 = Oper(procedure = divide_ints)
     self.assertTrue(oper1.isCommutative())
     self.assertFalse(oper2.isCommutative())
 
 """hasNeutral method is tested here (initialisation by graph)"""
   def test_neutral_elem_existance_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper1 = Operation(graph = graph1)
+    oper1 = Oper(graph = graph1)
     graph2 = {(0, 0): 0, (0, 1): 2, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper2 = Operation(graph = graph2)
+    oper2 = Oper(graph = graph2)
     self.assertTrue(oper1.hasNeutral())
     self.assertFalse(oper2.hasNeutral())
 
 """hasNeutral method is tested here (initialisation by procedure)"""
   def test_neutral_elem_existance_in_operation_initialised_by_procedure(self):
-    oper1 = Operation(procedure = sum_ints)
-    oper2 = Operation(procedure = divide_ints)
+    oper1 = Oper(procedure = sum_ints)
+    oper2 = Oper(procedure = divide_ints)
     self.assertTrue(oper1.hasNeutral())
     self.asserttrue(oper2.hasNeutral())
 
 """hasInverse method is tested here (initialisation by graph)"""
   def test_inverse_elem_existance_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
-    oper1 = Operation(graph = graph1)
+    oper1 = Oper(graph = graph1)
     graph2 = {(0, 0): 0, (0, 1): 1, (1, 0): 1, (1, 1): 1}
-    oper2 = Operation(graph = graph2)
+    oper2 = Oper(graph = graph2)
     self.assertTrue(oper1.hasInverse())
     self.assertFalse(oper2.hasInverse())
 
 """hasInverse method is tested here (initialisation by procedure)"""
   def test_inverse_elem_existance_in_operation_initialised_by_procedure(self):
-    oper1 = Operation(procedure = sum_ints)
-    oper2 = Operation(procedure = divide_ints)
+    oper1 = Oper(procedure = sum_ints)
+    oper2 = Oper(procedure = divide_ints)
     self.assertTrue(oper1.hasInverse())
     self.assertFalse(oper2.hasInverse())
