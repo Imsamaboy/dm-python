fa0a7550ad670247e54ec8dbe362c1a8e8f88c76
Alex-M-coder <55296215+Alex-M-coder@users.noreply.github.com>
2020-06-05 18:15:36 +0300

    TestOperation renaming

--- TestOperation.py
+++ TestOperation.py
@@ -8,7 +8,7 @@ class TestOperation(unittest.TestCase):
     return a / b
 
 """__contains__ method is tested here (initialisation by graph)"""
-  def test_checking_for_membership____graph(self):
+  def test_checking_for_membership_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
     oper1 = Operation(graph = graph1)
     for key in graph1
@@ -17,7 +17,7 @@ class TestOperation(unittest.TestCase):
     self.assertFalse(((1, 3), 1) in oper1)
 
 """__contains__ method is tested here (initialisation by procedure)"""
-  def test_checking_for_membership____procedure(self):
+  def test_checking_for_membership_in_operation_initialised_by_procedure(self):
     oper1 = Operation(procedure = sum_ints)
     for a in range(10)
       for b in range(10)
@@ -26,7 +26,7 @@ class TestOperation(unittest.TestCase):
 
 
 """__iter__ method is tested here (initialisation by graph)"""
-  def test_iterator_creating____graph(self):
+  def test_iterator_creating_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
     oper1 = Operation(graph = graph1)
 
@@ -41,7 +41,7 @@ class TestOperation(unittest.TestCase):
 
 
 """__call__ method is tested here (initialisation by graph)"""
-  def test_calling_operation_result____graph(self):
+  def test_calling_operation_result_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
     oper1 = Operation(graph = graph1)
 
@@ -53,7 +53,7 @@ class TestOperation(unittest.TestCase):
     self.assertEqual(results_of_operations, results_from_graph)
 
 """__call__ method is tested here (initialisation by procedure)"""
-  def test_calling_operation_result____procedure(self):
+  def test_calling_operation_result_in_operation_initialised_by_procedure(self):
     oper2 = Operation(procedure = sum_ints)
     for a in range(100)
       for b in range(100)
@@ -66,7 +66,7 @@ class TestOperation(unittest.TestCase):
 
 
 """isAssociative method is tested here (initialisation by graph)"""
-  def test_associativity_checking____graph(self):
+  def test_associativity_checking_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
     oper1 = Operation(graph = graph1)
     graph2 = {(0, 0): 0, (0, 1): 2, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
@@ -75,7 +75,7 @@ class TestOperation(unittest.TestCase):
     self.assertFalse(oper2.isAssociative())
 
 """isAssociative method is tested here (initialisation by procedure)"""
-  def test_associativity_checking____procedure(self):
+  def test_associativity_checking_in_operation_initialised_by_procedure(self):
     oper1 = Operation(procedure = sum_ints)
     oper2 = Operation(procedure = divide_ints)
     self.assertTrue(oper1.isAssociative())
@@ -83,7 +83,7 @@ class TestOperation(unittest.TestCase):
 
 
 """isCommutative method is tested here (initialisation by graph)"""
-  def test_commutativity_checking____graph(self):
+  def test_commutativity_checking_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
     oper1 = Operation(graph = graph1)
     graph2 = {(0, 0): 0, (0, 1): 2, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
@@ -92,14 +92,14 @@ class TestOperation(unittest.TestCase):
     self.assertFalse(oper2.isCommutative())
 
 """isCommutative method is tested here (initialisation by procedure)"""
-  def test_commutativity_checking____procedure(self):
+  def test_commutativity_checking_in_operation_initialised_by_procedure(self):
     oper1 = Operation(procedure = sum_ints)
     oper2 = Operation(procedure = divide_ints)
     self.assertTrue(oper1.isCommutative())
     self.assertFalse(oper2.isCommutative())
 
 """hasNeutral method is tested here (initialisation by graph)"""
-  def test_neutral_elem_existance____graph(self):
+  def test_neutral_elem_existance_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
     oper1 = Operation(graph = graph1)
     graph2 = {(0, 0): 0, (0, 1): 2, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
@@ -108,14 +108,14 @@ class TestOperation(unittest.TestCase):
     self.assertFalse(oper2.hasNeutral())
 
 """hasNeutral method is tested here (initialisation by procedure)"""
-  def test_neutral_elem_existance____procedure(self):
+  def test_neutral_elem_existance_in_operation_initialised_by_procedure(self):
     oper1 = Operation(procedure = sum_ints)
     oper2 = Operation(procedure = divide_ints)
     self.assertTrue(oper1.hasNeutral())
     self.asserttrue(oper2.hasNeutral())
 
 """hasInverse method is tested here (initialisation by graph)"""
-  def test_inverse_elem_existance____graph(self):
+  def test_inverse_elem_existance_in_operation_initialised_by_graph(self):
     graph1 = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2, (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
     oper1 = Operation(graph = graph1)
     graph2 = {(0, 0): 0, (0, 1): 1, (1, 0): 1, (1, 1): 1}
@@ -124,7 +124,7 @@ class TestOperation(unittest.TestCase):
     self.assertFalse(oper2.hasInverse())
 
 """hasInverse method is tested here (initialisation by procedure)"""
-  def test_inverse_elem_existance____procedure(self):
+  def test_inverse_elem_existance_in_operation_initialised_by_procedure(self):
     oper1 = Operation(procedure = sum_ints)
     oper2 = Operation(procedure = divide_ints)
     self.assertTrue(oper1.hasInverse())
